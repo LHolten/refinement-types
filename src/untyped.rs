55748e2f9f75b33181318fa6a7bdaa01bff1015f
Lucas Holten <lhc.holten@gmail.com>
2023-02-25 23:20:01 +0100

    Still with place

--- src/untyped.rs
+++ src/untyped.rs
@@ -1,8 +1,9 @@
 use std::rc::Rc;
 
 // Globally unique
+// TODO: override to use ptr comparisons
 #[derive(Debug, PartialEq, Eq, Hash)]
-struct Variant(Rc<()>);
+pub struct Variant(Rc<()>);
 
 // this defines the shape of a value
 // it has holes that can be filled with continuations
@@ -12,17 +13,17 @@ enum Pat {
 }
 
 // the actual content of a value, it fills the holes of a Pat
-enum Subst {
+pub enum Subst {
     Vals(Vec<Subst>),
     Con(Rc<Con>),
 }
 
 /// selector for a continuation in a Subst
-struct Place {
-    pos: usize,
-    then: MaybePlace,
+pub struct Place {
+    pub pos: usize,
+    pub then: MaybePlace,
 }
-type MaybePlace = Option<Rc<Place>>;
+pub type MaybePlace = Option<Rc<Place>>;
 
 impl Subst {
     fn app_sub(&self, place: &MaybePlace) -> Rc<Con> {
@@ -42,89 +43,60 @@ impl Subst {
 }
 
 // The value; it contains a pattern and a substitution
-struct Value {
+pub struct Value {
     pat: Rc<Pat>,
     subst: Rc<Subst>,
 }
 
 // continuations map patterns to programs without environment
-struct Con {
-    map: Rc<dyn Fn(usize, &Pat) -> Expr>,
+pub struct Con {
+    pub map: Rc<dyn Fn(&Pat) -> Expr>,
+    pub env: Rc<Env>,
 }
 
 impl Con {
-    fn app_con(&self, offset: usize, pat: &Pat) -> Expr {
-        (self.map)(offset, pat)
+    fn app_con(&self, pat: &Pat) -> Expr {
+        (self.map)(pat)
     }
 }
 
 // an actual program, without environment
-struct Expr {
-    idx: usize,
-    place: MaybePlace,
-    val: Value,
+pub struct Expr {
+    pub idx: usize,
+    pub place: MaybePlace,
+    pub val: Value,
 }
 
-struct Env {
-    items: Vec<Rc<Subst>>,
+pub struct Prog {
+    pub env: Rc<Env>,
+    pub body: Expr,
 }
+
+pub struct Env {
+    subst: Rc<Subst>,
+    then: MaybeEnv,
+}
+pub type MaybeEnv = Option<Rc<Env>>;
+
 impl Env {
     fn lookup(&self, idx: usize, place: &MaybePlace) -> Rc<Con> {
-        self.items[idx].app_sub(place)
+        match idx.checked_sub(1) {
+            Some(idx) => self.then.unwrap().lookup(idx, place),
+            None => self.subst.app_sub(place),
+        }
     }
-}
-
-struct Prog {
-    env: Env,
-    body: Expr,
 }
 
 impl Prog {
     fn eval(self) -> Self {
         let Prog { mut env, body } = self;
         let Expr { idx, place, val } = body;
-        let body = env.lookup(idx, &place).app_con(idx, &val.pat);
-
-        let mut items = vec![val.subst];
-        items.extend(env.items);
-        env.items = items;
+        let con = env.lookup(idx, &place);
+        let env = Rc::new(Env {
+            subst: val.subst,
+            then: Some(con.env),
+        });
+        let body = (con.map)(&val.pat);
         Self { env, body }
     }
 }
-
-// fn is_even(idx: usize, place: Place, pat: &Pat) -> Expr {
-//     match pat {
-//         Pat::Z => Expr {
-//             idx: idx + 1,
-//             place,
-//             val: Value {
-//                 pat: Rc::new(Pat::TT),
-//                 subst: Rc::new(Subst::Nil),
-//             },
-//         },
-//         Pat::S(n) => match n.as_ref() {
-//             Pat::Z => Expr {
-//                 idx: idx + 1,
-//                 place,
-//                 val: Value {
-//                     pat: Rc::new(Pat::TT),
-//                     subst: Rc::new(Subst::Nil),
-//                 },
-//             },
-//             Pat::S(n) => is_even(idx, place, n),
-//             _ => panic!(),
-//         },
-//         _ => panic!(),
-//     }
-// }
-
-// fn branch(then: Con, other: Con, pat: &Pat) -> Expr {
-//     match pat {
-//         Pat::TT => todo!(),
-//         Pat::FF => todo!(),
-//         _ => panic!(),
-//     }
-// }
-// pub fn test() {
-//     let prof
-// }
